1ef4ec6238192fc716ccc8de1984a982c3972d98
AzmainIsraq <56102572+AzmainIsraq@users.noreply.github.com>
2019-10-09 13:22:52 +0600

    First commit
    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const figlet = require('figlet');
+const colors = require('colors');
+const readline = require('readline');
+const commando = require(`discord.js-commando`);
+
+const config = require('./config.json');
+const bot = new commando.Client({
+    commandPrefix:'ffbd'
+});
+
+const cmdsArray = [
+    "dmall <message>",
+    "dmrole <role> <message>"
+];
+
+bot.on("ready", () => {
+    clear();
+    console.log('______')
+});
+
+
+bot.on("error", (error) => {
+    bot.login(config.token);
+});
+
+bot.registry.registerGroup('dms', 'help');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+if (process.env.BOT_TOKEN) bot.login(process.env.BOT_TOKEN);
+else bot.login(config.token);
+
+
+
+
+function clear() {
+    console.clear();
+    console.log(figlet.textSync("FFBD Bot").red);
+    console.log("\n\nFFBD Discord Bot. \n Sends DMs to selected members of guild.\n  FFBD Official bot.");
+    console.log(`\nRandom send time set @ 0.01-${config.wait}s`);
+    console.log(` Type  ${config.prefix}help  in a chat.\n\n`);
+}
